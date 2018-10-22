a1ba698c23ce79243dd0d1908df49d622c134b20
Jeanmartin <>
2018-10-22 17:55:14 -0500

    Añadiendo Sp_Registrar_Contacto
    Se añade el siguiente procedimiento almacenado.

--- Proyecto/BD/Inserts.sql
+++ Proyecto/BD/Inserts.sql
@@ -2,7 +2,7 @@
 USE BD_WebFoched;
 
 /*****************************************************
-Autor: Pachas Villa Jeanmartin
+Autor: Pachas Villa Jesus Jeanmartin
 Descripción: Resgistrar Restaurante
 Fecha Actualizacion: 22/10/2018
 Ejecutar: CALL sp_registrar_restaurante('','','','','','')
@@ -23,5 +23,37 @@ BEGIN
                           VALUES(_Razon_Social,_RUC_DNI,_Logo_Imagen
                             ,_Direccion_Red_Social,_usuario,_PC
                           ,NOW());
+END$$
+DELIMITER ;
+
+
+/*****************************************************
+Autor: Pachas Villa  Jesus Jeanmartin
+Descripción: Resgistrar Contacto
+Fecha Actualizacion: 22/10/2018
+Ejecutar: CALL sp_registrar_contacto('','','','','','','','','','')
+******************************************************/
+DELIMITER $$
+CREATE PROCEDURE sp_registrar_contacto(
+  	_Id_Restaurante INT
+  ,	_Rol TINYINT
+  ,	_Nombres	VARCHAR(100)
+  ,	_Apellidos VARCHAR(100)
+  ,	_Numero_1 VARCHAR(11)
+  ,	_Numero_2 VARCHAR(11)
+  ,	_Numero_3 VARCHAR(11)
+  ,	_Direccion_Red_Social VARCHAR(200)
+  , _usuario     VARCHAR(30)
+  , _PC				  VARCHAR(30)
+)
+BEGIN
+	INSERT INTO Contacto (Id_Restaurante,Rol,Nombres
+                          ,Apellidos,Numero_1,Numero_2
+                          ,Numero_3,Direccion_Red_Social,Usuario_Creacion
+                        ,PC_Creacion,Fecha_Creacion)
+                          VALUES(_Id_Restaurante,_Rol,_Nombres
+                                ,_Apellidos,_Numero_1,_Numero_2
+                                ,_Numero_3,_Direccion_Red_Social,_usuario
+                                                ,_PC,NOW());
 END$$
 DELIMITER ;
