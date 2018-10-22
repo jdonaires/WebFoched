aee45fee0ebb5d4b0787a0abcd2b988d37a2d9a8
Antony <44219719+AntonyGuillen@users.noreply.github.com>
2018-10-22 18:03:03 -0500

    Añadiendo campos a la Tabla Categoria
    se añadieron los siguientes campos ya que la tabla se a actualizado

--- Proyecto/BOL/Categoria.php
+++ Proyecto/BOL/Categoria.php
@@ -4,6 +4,9 @@ class Categoria
 	private $Id_Categoria;
 	private $Nombre;
   private $Descripcion;
+	private $Usuario_Creacion
+	private $PC_Creacion
+	private $Fecha_Creacion
 	private $Estado;
 
 	public function __GET($x)
