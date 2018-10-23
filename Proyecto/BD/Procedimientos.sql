0c692f7bdc2d6cdbe90b1ded6c341a59640842f5
SharonSalcedo <44219667+SharonSalcedo@users.noreply.github.com>
2018-10-23 13:23:30 -0500

    Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/jdonaires/WebFoched""""
    This reverts commit 9c62ded10f9dbbceb3d01f4cf53e5edc63c744fa.

--- Proyecto/BD/Procedimientos.sql
+++ Proyecto/BD/Procedimientos.sql
@@ -3,19 +3,19 @@ USE BD_WebFoched;
 /*****************************************************
 Autor: Ever Néstarez Martinez
 Descripción: Resgistrar Usuarios
-Fecha Actualizacion: 20/10/2018
-Ejecutar: CALL sp_registrar_usuario('usuario','pass','correo@mail.com')
+Fecha Actualizacion: 23/10/2018
+Ejecutar: CALL sp_registrar_usuario('usuario1','pass','correo@mail.com');
 ******************************************************/
 DELIMITER $$
 CREATE PROCEDURE sp_registrar_usuario(
 	_usuario 	VARCHAR(20),
 	_pass 		VARCHAR(20),
-	_correo 	VARCHAR(25),
-	_usuario  VARCHAR(30),
-	_PC				VARCHAR(30),
+	_correo 	VARCHAR(25)
 )
 BEGIN
-/*Esta es la respuesta de porque el campo default para no agregar EL CAMPO ESTADO en el SP*/
-	INSERT INTO Usuario (usuario,pass,correo,Usuario_Creacion,PC_Creacion,Fecha_Creacion) VALUES(_usuario,_pass,_correo,_usuario,_PC,NOW());
+	SET @fecha = (SELECT NOW());
+	/*Esta es la respuesta de porque el campo default para no agregar EL CAMPO ESTADO en el SP*/
+	INSERT INTO Usuario (usuario,pass,correo,Fecha_Creacion) VALUES(_usuario,_pass,_correo,_PC,@fecha);
 END$$
 DELIMITER ;
+
