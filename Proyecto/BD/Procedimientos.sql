b6cbf686df4d7fe100dc9511a3da40328df43602
EverNestarez <37964310+EverNestarez@users.noreply.github.com>
2018-10-23 13:09:19 -0500

    Errores Resueltos DB
    Algunos Errores Corregidos en la db

--- Proyecto/BD/Procedimientos.sql
+++ Proyecto/BD/Procedimientos.sql
@@ -3,19 +3,21 @@ USE BD_WebFoched;
 /*****************************************************
 Autor: Ever Néstarez Martinez
 Descripción: Resgistrar Usuarios
-Fecha Actualizacion: 20/10/2018
-Ejecutar: CALL sp_registrar_usuario('usuario','pass','correo@mail.com')
+Fecha Actualizacion: 23/10/2018
+Ejecutar: CALL sp_registrar_usuario('Ever','pass','correo@mail.com','user1','PC-5A24C');
 ******************************************************/
 DELIMITER $$
 CREATE PROCEDURE sp_registrar_usuario(
 	_usuario 	VARCHAR(20),
 	_pass 		VARCHAR(20),
 	_correo 	VARCHAR(25),
-	_usuario  VARCHAR(30),
-	_PC				VARCHAR(30),
+	_usuario_db  	VARCHAR(30),
+	_PC		VARCHAR(30)
 )
 BEGIN
-/*Esta es la respuesta de porque el campo default para no agregar EL CAMPO ESTADO en el SP*/
-	INSERT INTO Usuario (usuario,pass,correo,Usuario_Creacion,PC_Creacion,Fecha_Creacion) VALUES(_usuario,_pass,_correo,_usuario,_PC,NOW());
+	SET @fecha = (SELECT NOW());
+	/*Esta es la respuesta de porque el campo default para no agregar EL CAMPO ESTADO en el SP*/
+	INSERT INTO Usuario (usuario,pass,correo,Usuario_Creacion,PC_Creacion,Fecha_Creacion) VALUES(_usuario,_pass,_correo,_usuario_db,_PC,@fecha);
 END$$
 DELIMITER ;
+
