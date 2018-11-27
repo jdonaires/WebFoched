3fd29b874a333bb13bdf0940e83cb07531357ab3
jeanmartin75065904 <jeanmartin.05@hotmail.com>
2018-11-27 17:40:26 -0500

    Formulario Registrar Productos
    Para registrar bebidas y platos de los puntos de ventas.

--- Proyecto/DESIGNER/producto.php
+++ Proyecto/DESIGNER/producto.php
@@ -2,3 +2,28 @@
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Productos</title>
+    <link href="assets/css/estiloF.css" rel="stylesheet" />
+  </head>
+  <body>
+    <center>
+    <div id='container'>
+      <div class='signup'>
+         <form action="" method="post">
+           <h1>Productos</h1>
+           <select class="" name="">
+             <option value="">Bebidas</option>
+             <option value="">Platos</option>
+           </select>
+           <input type='text' name="txt_nombre" placeholder='Nombre'  />
+           <input type='text' name="txt_precio" placeholder='Precio: 0.00'  />
+           <input type="submit" name="btn_registrar" placeholder='SUBMIT' value="Registrar ">
+         </form>
+    </center>
+
+  </body>
+</html>
