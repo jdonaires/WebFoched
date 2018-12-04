26039249bc8b8cfc5796f8037128ebb7e7f4d289
SharonSalcedo <44219667+SharonSalcedo@users.noreply.github.com>
2018-12-04 14:45:44 -0500

    Buscar
    Creacion de Buscar

---
+++ Proyecto/DESIGNER/buscar.php
@@ -0,0 +1,83 @@
+<?php require ('InicioSesion.php'); ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Fooched</title>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, user-scalable=no"
+    />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/restaurantes.css" />
+
+
+    <script src="alertify/alertify.js"></script>
+    <link rel="stylesheet" type="text/css" href="alertify/css/alertify.css">
+    <link rel="stylesheet" type="text/css" href="alertify/css/themes/bootstrap.css">
+
+  </head>
+  <body class="landing is-preload">
+    <div id="page-wrapper">
+      <!-- Header -->
+      <header id="header" class="alt">
+
+        <?php require_once ('nav.php'); ?>
+
+      </header>
+
+      <!-- Banner -->
+      <section id="banner">
+        <h2>Fooched</h2>
+        <p>Encuentra El Mejor Establecimiento De Comida, Al Mejor Precio.</p>
+
+      </section>
+      
+      <!-- Footer -->
+      <footer id="footer">
+        <ul class="icons">
+          <li>
+            <a href="#" class="icon fa-twitter"
+              ><span class="label">Twitter</span></a
+            >
+          </li>
+          <li>
+            <a href="#" class="icon fa-facebook"
+              ><span class="label">Facebook</span></a
+            >
+          </li>
+          <li>
+            <a href="#" class="icon fa-instagram"
+              ><span class="label">Instagram</span></a
+            >
+          </li>
+          <li>
+            <a href="#" class="icon fa-github"
+              ><span class="label">Github</span></a
+            >
+          </li>
+          <li>
+            <a href="#" class="icon fa-dribbble"
+              ><span class="label">Dribbble</span></a
+            >
+          </li>
+          <li>
+            <a href="#" class="icon fa-google-plus"
+              ><span class="label">Google+</span></a
+            >
+          </li>
+        </ul>
+      </footer>
+      </div>
+      <!-- Scripts -->
+      <script src="assets/js/jquery.min.js"></script>
+      <script src="assets/js/jquery.dropotron.min.js"></script>
+      <script src="assets/js/jquery.scrollex.min.js"></script>
+      <script src="assets/js/browser.min.js"></script>
+      <script src="assets/js/breakpoints.min.js"></script>
+      <script src="assets/js/util.js"></script>
+      <script src="assets/js/main.js"></script>
+      </body>
+      </html>
