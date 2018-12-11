0aac3173c86b30c8d19b5520a7606bacf3f3af99
Jeanmartin <jeaanmartin.05@hotmail.com>
2018-12-11 12:50:18 -0500

    AGREGO EL SELECT PARA LA WEB
    Se agrega el combobox que carga el select

--- Proyecto/DESIGNER/buscar.php
+++ Proyecto/DESIGNER/buscar.php
@@ -1,4 +1,9 @@
-<?php require ('InicioSesion.php'); ?>
+
+<?php require ('InicioSesion.php');
+include_once "../DAO/UbicacionDAO.php";
+$ubicacionDAO = new UbicacionDAO();
+?>
+
 
 <!DOCTYPE html>
 <html>
@@ -12,13 +17,14 @@
     <link rel="stylesheet" href="assets/css/styleModal.css" />
     <link rel="stylesheet" href="assets/css/main.css" />
     <link rel="stylesheet" href="assets/css/style.css" />
-    
-    
+
+
+
     <script src="alertify/alertify.js"></script>
     <link rel="stylesheet" type="text/css" href="alertify/css/alertify.css">
     <link rel="stylesheet" type="text/css" href="alertify/css/themes/bootstrap.css">
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRCleE2CV2kBgWDWmOMQMyvSHrXifc1WA"></script>
-    
+
   </head>
   <body class="landing is-preload">
     <div id="page-wrapper">
@@ -27,18 +33,25 @@
         <?php require_once ('nav.php'); ?>
 
       </header>
-      
+
       <!-- Banner -->
       <section id="banner">
         <h2>Fooched</h2>
         <p>Encuentra el mejor establecimiento de comida al mejor precio</p>
+        <p>Despliega la barra y selecciona al lugar donde quieres ir, para generar la ruta:</p>
+        <select class="txt" onchange="draw_rute(this.value)">
+      						<option value="0">Rutas &#8595;</option>
+      						<?=$ubicacionDAO->GET_PUNTOS_VENTAS();?>
+      					</select>
+            <p>Presiona el boton para habilitar el mapa:</p>
         <ul class="actions special">
           <li><button class="button" id="mostrarEstablecimientos">Ver el mapa de establecimientos en Chincha</button></li>
         </ul>
         <section id="map"></section>
       </section>
-      
-      
+
+
+
       <!-- Main -->
       <section id="main" class="container">
       <div class="campo-busqueda">
@@ -50,7 +63,7 @@
                         </span>
                       </div>
                     </div>
-                    
+
                 <div class="columns large-5 pad0">
                   <div class="campo-tipo campo-show wrap-input wrap-input-first-half columns medium-6">
                     <label for="tipo_cocina"><span>¿Donde?¿Qué tipo de cocina?</span></label>
@@ -83,36 +96,36 @@
             </div>
           </div>
       </section>
-      
+
       <section id="list-restaurant" class="box special list">
-        
+
         <div class="container">
           <div class="row">
             <div class="col-3  fh5co-item-wrap animate-box">
               <div class="fh5co-item">
                 <img src="images/pic02.jpg" class="img-restaurant" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
             <h3>Bake Potato Pizza</h3>
-            
+
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
 <a href="cateRestaurantes.php" class="button-rest">Ver</a><!--primer cambio-->
 </div>
         </div>
-        
+
         <div class="col-3  fh5co-item-wrap animate-box">
           <div class="item">
             <img src="images/pic02.jpg" class="img-restaurant" alt="">
             <h3>Restaurant</h3>
-            
+
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
             <a href="cateRestaurantes.php" class="button-rest">Ver</a><!--primer cambio-->
           </div>
         </div>
-        
+
         <div class="col-3  fh5co-item-wrap animate-box">
           <div class="item">
             <img src="images/pic02.jpg" class="img-restaurant" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
             <h3>Restaurant</h3>
-            
+
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
             <a href="cateRestaurantes.php" class="button-rest">Ver</a><!--primer cambio-->
           </div>
@@ -123,11 +136,11 @@
             <h3>Restaurant</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
             <a href="cateRestaurantes.php" class="button-rest">Ver</a><!--primer cambio-->
-            
+
           </div>
         </div>
       </div>
-      
+
       </section>
     </section>
 
