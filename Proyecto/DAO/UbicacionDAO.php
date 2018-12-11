5b94115ca7db5fd16aa9c51a426507e003d623fd
Jeanmartin <jeaanmartin.05@hotmail.com>
2018-12-11 12:46:37 -0500

    Revert "FUNCION PARA RETORNAR LOS PUNTOS DE VENTAS"
    This reverts commit a5ed3a9cdffa152a05e53f0eb453d93210d43ad8.

--- Proyecto/DAO/UbicacionDAO.php
+++ Proyecto/DAO/UbicacionDAO.php
@@ -10,7 +10,7 @@ class UbicacionDAO
 			$dba = new DBAccess();
 			$this->pdo = $dba->get_connection();
     }
-
+    
     public function GET_UBICACION_NEGOCIOS(Ubicacion $ubicacion)
     {
         try
@@ -18,7 +18,7 @@ class UbicacionDAO
             $statement = $this->pdo->prepare("CALL SP_GET_UBICACION_NEGOCIOS(?)");
             $statement->bindValue(1,$ubicacion->__GET('Distrito'),PDO::PARAM_STR);
             $statement -> execute();
-            $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+            $result = $statement->fetchAll(PDO::FETCH_ASSOC);    
 
             return $result;
         }
@@ -27,19 +27,5 @@ class UbicacionDAO
             die($ex->getMessage());
         }
     }
-
-    public function GET_PUNTOS_VENTAS(){
-      $statement = $this->pdo->prepare("CALL CALL_SP_BUSCAR_RESTAURANTE()");
-      $statement -> execute();
-       $option = '';
-       foreach ($statement as $key){
-         $id = $key['Id_Restaurante'];
-         $name = $key['Razon_Social'];
-         $option .= '<option value="'.$id.'">'.$name.' - B</option>';
-       }
-       return $option;
-     }
-
-
 }
 ?>
